aa23de1e2be50211e4266cc61090635e99588085
StyleShit <lurkers2030@gmail.com>
2022-06-02 21:45:53 +0300

    Refactor Toast component

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,21 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import './css/Toast.css';
 
 export default function Toast( { type, text } ) {
-	const toastElement = useRef();
-
-	useEffect( () => {
-		const timeout = setTimeout( () => {
-			toastElement.current.remove();
-		}, 5000 );
-
-		return () => {
-			clearTimeout( timeout );
-		};
-	}, [ type ] );
-
 	return (
-		<div ref={ toastElement } className={ `toast toast-${ type }` }>
+		<div
+			onAnimationEnd={ ( e ) => e.currentTarget.remove() }
+			className={ `toast toast-${ type }` }
+		>
 			{ text }
 		</div>
 	);
